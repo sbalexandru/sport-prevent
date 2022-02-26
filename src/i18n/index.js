c6dff1a1e8fa116e117e112c1f53b00800101d86
sbalexandru <sbalexandru@yahoo.com>
2022-02-26 14:54:15 +0200

    uninstal i18next-http-backend

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,7 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-// import HttpApi from "i18next-http-backend";
 
 import ro from "./ro.json";
 import hu from "./hu.json";
@@ -16,7 +15,6 @@ const resources = {
 i18n
   .use(initReactI18next)
   .use(LanguageDetector)
-  // .use(HttpApi)
   .init({
     resources,
     supportedLngs: ["en", "hu", "ro"],
@@ -26,7 +24,6 @@ i18n
       order: ["cookie", "htmlTag", "localStorage", "path", "subdomain"],
       caches: ["cookie"],
     },
-    // react: { useSuspense: false },
   });
 
 export default i18n;
